513e6a62ced4f3e99d0fc42d360e601153c895ac
Rylan Graham <95762889+RylanJGraham@users.noreply.github.com>
2022-10-25 11:17:25 +0200

    Tilesets

---
+++ DevelopmentProjFinal/TexturesGame/Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.5" name="Grass/Tiles" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="49" columns="7">
+ <image source="TX Tileset Grass.png" width="256" height="256"/>
+</tileset>
