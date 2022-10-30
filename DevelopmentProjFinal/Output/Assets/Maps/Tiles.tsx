c2313960b4461c10ba4a178886a81c4bdd8b5c61
RylanJGraham <95762889+RylanJGraham@users.noreply.github.com>
2022-10-30 17:08:09 +0100

    mapupdates

--- DevelopmentProjFinal/Output/Assets/Maps/Tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.5" name="Grass/Tiles" tilewidth="32" tileheight="32" tilecount="64" columns="8">
- <image source="TX Tileset Grass.png" width="256" height="256"/>
-</tileset>
