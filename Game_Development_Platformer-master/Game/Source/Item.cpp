e259ee8f54b6f6be313d9dd51b7a017f784194a8
StarvinXarvin <99918767+StarvinXarvin@users.noreply.github.com>
2023-01-28 23:48:57 +0100

    Chests turned static, K debug key for killing the player, J debug key for hurting the player without substracting hp

--- Game_Development_Platformer-master/Game/Source/Item.cpp
+++ Game_Development_Platformer-master/Game/Source/Item.cpp
@@ -36,7 +36,7 @@ bool Item::Start() {
 	texture = app->tex->Load(texturePath);
 	
 	// L07 DONE 4: Add a physics to an item - initialize the physics body
-	pbody = app->physics->CreateCircle(position.x + 16, position.y + 16, 16, bodyType::DYNAMIC, ColliderType::ITEM);
+	pbody = app->physics->CreateCircle(position.x + 16, position.y + 16, 16, bodyType::STATIC, ColliderType::ITEM);
 	pbody->cType = ColliderType::ITEM;
 	pbody->listener = this;
 	isPicked = false;
